47ce26e5c02d6b7d3c02ada43fcdb22b3757cd55
Quốc Chương <quocchuong2609@gmail.com>
2025-01-24 15:30:35 +0700

    update: code demo for activity lifecycle

--- slot-5/app/src/main/java/com/example/slot5/LifecycleActivity.kt
+++ slot-5/app/src/main/java/com/example/slot5/LifecycleActivity.kt
@@ -1,15 +1,45 @@
 package com.example.slot5
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
 
 class LifecycleActivity : ComponentActivity() {
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Text(text = "This is Lifecycle Activity!")
-        }
+        Log.d(TAG, "onCreate called")
+        // Initialize UI components and variables
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart called")
+        // Activity becomes visible to the user
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume called")
+        // Activity starts interacting with the user
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause called")
+        // Save state or stop resource-intensive tasks
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop called")
+        // Perform cleanup tasks or save states
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
+        // Release resources and clean up connections
     }
 }
