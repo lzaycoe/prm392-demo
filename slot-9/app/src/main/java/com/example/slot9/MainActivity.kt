43839798910314e32cf2b08ab04e4b5d5a6f29eb
linhlm0210 <lammylinh7012@gmail.com>
2025-02-15 18:31:22 +0700

    add: code demo

--- slot-9/app/src/main/java/com/example/slot9/MainActivity.kt
+++ slot-9/app/src/main/java/com/example/slot9/MainActivity.kt
@@ -4,24 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.slot9.ui.theme.Slot9Theme
 
+
+
+// MainActivity - Controller
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // Initialize repository
+        val userRepository = UserRepository()
+        val users = userRepository.getUsers()
+
         setContent {
             Slot9Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    UserList(
+                        users = users,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +39,21 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// View
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Slot9Theme {
-        Greeting("Android")
+fun UserList(users: List<User>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier.fillMaxSize()) {
+        items(users) { user ->
+            UserItem(user = user)
+        }
     }
 }
+
+@Composable
+fun UserItem(user: User, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(text = "Name: ${user.name}")
+        Text(text = "Email: ${user.email}")
+    }
+}
+
