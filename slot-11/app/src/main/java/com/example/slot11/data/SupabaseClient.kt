e20687f9acfc7be261ffcfa0e2fd8bdb827f64df
Thunder <htn10a2@gmail.com>
2025-02-25 13:36:59 +0700

    add: create supabase client

---
+++ slot-11/app/src/main/java/com/example/slot11/data/SupabaseClient.kt
@@ -0,0 +1,12 @@
+import com.example.slot11.BuildConfig
+import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.postgrest.Postgrest
+
+val supabase = createSupabaseClient(
+    supabaseUrl = BuildConfig.SUPABASE_URL,
+    supabaseKey = BuildConfig.SUPABASE_KEY
+) {
+    install(Auth)
+    install(Postgrest)
+}
