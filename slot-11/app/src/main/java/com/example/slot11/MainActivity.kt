9f905feba4512eb490c3d975c5b13631598e7c58
Thunder <htn10a2@gmail.com>
2025-02-25 13:39:02 +0700

    update: implement ui and navigation

--- slot-11/app/src/main/java/com/example/slot11/MainActivity.kt
+++ slot-11/app/src/main/java/com/example/slot11/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.slot11.data.repository.AuthRepository
+import com.example.slot11.ui.screens.HomeScreen
+import com.example.slot11.ui.screens.LoginScreen
+import com.example.slot11.ui.screens.SignupScreen
 import com.example.slot11.ui.theme.Slot11Theme
+import com.example.slot11.viewmodel.AuthViewModel
+import com.example.slot11.viewmodel.AuthViewModelFactory
+import supabase
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +28,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Slot11Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Column (
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    App()
                 }
             }
         }
@@ -31,17 +40,57 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun App() {
+    val navController = rememberNavController()
+    val authRepository = AuthRepository(supabase)
+    val viewModel: AuthViewModel = viewModel(factory = AuthViewModelFactory(authRepository))
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Slot11Theme {
-        Greeting("Android")
+    LaunchedEffect(viewModel.isLoggedIn.value) {
+        if (viewModel.isLoggedIn.value) {
+            navController.navigate("home") {
+                popUpTo("login") {
+                    inclusive = true
+                }
+            }
+        } else {
+            navController.navigate("login") {
+                popUpTo("home") {
+                    inclusive = true
+                }
+            }
+        }
+    }
+
+    NavHost(
+        navController = navController,
+        startDestination = "login"
+    ) {
+        composable("login") {
+            LoginScreen(
+                viewModel = viewModel,
+                onNavigateToSignUp = {
+                    navController.navigate("signup")
+                }
+            )
+        }
+
+        composable("signup") {
+            SignupScreen(
+                viewModel = viewModel,
+                onNavigateToLogin = {
+                    navController.navigate("login")
+                }
+            )
+        }
+
+        composable("home") {
+            HomeScreen(
+                viewModel = viewModel,
+                navController = navController
+            )
+        }
     }
 }
+
+
+
